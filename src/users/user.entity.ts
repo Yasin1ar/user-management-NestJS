b59c3226234df46c224485a6580282686799d01f
Yasin Amirany <yasin@amirany.net>
2025-04-29 16:00:43 +0330

    add user entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  username: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    default: 'user',
+    nullable: false,
+    enum: ['user', 'admin'],
+  })
+  role: 'user' | 'admin';
+}
