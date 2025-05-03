ac4313abaa93f754595c1ba2531a700fbbc199a9
Yasin Amirany <yasin@amirany.net>
2025-05-03 12:33:18 +0330

    set up config for env management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,12 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
