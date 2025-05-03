cd0676d4c6ff03b896a1e0d62fdbe702515e8158
Yasin Amirany <yasin@amirany.net>
2025-05-03 08:17:00 +0330

    add auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
       synchronize: true, // Set to false in production!
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
