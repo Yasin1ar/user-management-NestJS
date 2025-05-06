afb94acd05704b07fc8e7fd0764eaf4eb1bdbd92
Yasin Amirany <yasin@amirany.net>
2025-05-06 10:28:26 +0330

    update user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,23 +1,23 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ nullable: false })
+  @Column({ nullable: false, length: 255, unique: true })
   username: string;
 
-  @Column({ nullable: false })
+  @Column({ nullable: false, length: 255 })
   password: string;
 
   @Column({
-    type: 'varchar',
-    default: 'user',
-    nullable: false,
+    type: 'enum',
     enum: ['user', 'admin'],
+    default: 'user'
   })
   role: 'user' | 'admin';
+
   @Column({ type: 'text', nullable: true })
   refreshToken: string | null;
 }
