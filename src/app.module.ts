0662906f81fdbed29df327600dd2d05f4ba934ac
Yasin Amirany <yasin@amirany.net>
2025-05-08 23:24:12 +0330

    update app mod to have variables for creds instead of hardcoding

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest_db',
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Set to false in production!
+      synchronize: process.env.NODE_ENV !== 'production', // Only true in development
     }),
     UsersModule,
     AuthModule,
