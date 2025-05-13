b7b54339d4ea6743210f3439ab375385b02582d8
Yasin Amirany <yasin@amirany.net>
2025-05-13 12:03:31 +0330

    add roles service

---
+++ src/roles/roles.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Role } from '../users/role.entity';
+import { Permission } from '../users/permission.entity';
+import { CreateRoleDto, UpdateRoleDto } from './dto';
+
+@Injectable()
+export class RolesService {
+  constructor(
+    @InjectRepository(Role)
+    private roleRepository: Repository<Role>,
+    @InjectRepository(Permission)
+    private permissionRepository: Repository<Permission>,
+  ) {}
+
+  async create(createRoleDto: CreateRoleDto): Promise<Role> {
+    const role = this.roleRepository.create(createRoleDto);
+    
+    if (createRoleDto.permissionIds) {
+      role.permissions = await this.permissionRepository.findByIds(
+        createRoleDto.permissionIds,
+      );
+    }
+
+    return this.roleRepository.save(role);
+  }
+
+  async findAll(): Promise<Role[]> {
+    return this.roleRepository.find({ relations: ['permissions'] });
+  }
+
+  async findOne(id: number): Promise<Role> {
+    const role = await this.roleRepository.findOne({ 
+      where: { id },
+      relations: ['permissions'] 
+    });
+
+    if (!role) {
+      throw new NotFoundException(`Role with ID ${id} not found`);
+    }
+
+    return role;
+  }
+
+  async update(id: number, updateRoleDto: UpdateRoleDto): Promise<Role> {
+    const role = await this.findOne(id);
+    
+    if (updateRoleDto.permissionIds) {
+      role.permissions = await this.permissionRepository.findByIds(
+        updateRoleDto.permissionIds,
+      );
+    }
+
+    Object.assign(role, updateRoleDto);
+    return this.roleRepository.save(role);
+  }
+
+  async remove(id: number): Promise<void> {
+    const result = await this.roleRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Role with ID ${id} not found`);
+    }
+  }
+}
