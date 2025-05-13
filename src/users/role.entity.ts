dfb6686e5052e2d15b1bb8abf01d5346b4438b94
Yasin Amirany <yasin@amirany.net>
2025-05-13 09:14:38 +0330

    add role entity

---
+++ src/users/role.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from 'typeorm';
+import { User } from './user.entity';
+import { Permission } from './permission.entity';
+
+@Entity()
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column()
+  description: string;
+
+  @ManyToMany(() => User, user => user.roles)
+  users: User[];
+
+  @ManyToMany(() => Permission, permission => permission.roles)
+  @JoinTable()
+  permissions: Permission[];
+}
